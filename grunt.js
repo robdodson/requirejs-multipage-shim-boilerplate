3ec48110233d6295e9b30193409ecda4add8dc25
Rob Dodson <rob.dodson@ge.com>
2012-10-22 16:06:08 -0700

    added grunt build tasks

--- grunt.js
+++ grunt.js
@@ -1,30 +1,38 @@
 /*global module:false*/
 'use strict';
 
+var opt = require('./options');
+
 module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
 
-    // Project configuration.
-    grunt.initConfig({
-        pkg: '<json:package.json>',
+    clean: {
+      folder: 'www-release'
+    },
 
-        clean: {
-            folder: 'www-release'
-        },
+    requirejs: {
+      compile: {
+        options: opt
+      }
+    },
 
-        mincss: {
-            dist: {
-                files: {
-                  
-                }
-            }
+    mincss: {
+      compile: {
+        files: {
+          'www-release/css/app/style.css': 'www-release/css/app/style.css'
         }
-    });
+      }
+    }
+  });
 
-    // Load tasks from NPM
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-mincss');
+  // Load tasks from NPM
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-mincss');
 
-    // Default task.
-    grunt.registerTask('default', 'clean less mincss copy');
+  // Default task.
+  grunt.registerTask('default', 'clean requirejs mincss');
 
 };
