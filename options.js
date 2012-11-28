874e295c30510223c5c6fe6d964c8b18c630f4df
Rob Dodson <lets.email.rob@gmail.com>
2012-11-27 20:25:28 -0800

    cleaned up some language

--- options.js
+++ options.js
@@ -4,45 +4,34 @@ module.exports = {
   mainConfigFile: 'www/js/common.js',
   dir: 'www-release',
   modules: [
-    //First set up the common build layer.
+    // First set up the common build layer.
     {
-      //module names are relative to baseUrl
+      // module names are relative to baseUrl
       name: 'common',
-      //List common dependencies here. Only need to list
-      //top level dependencies, 'include' will find
-      //nested dependencies.
+      // List common dependencies here. Only need to list
+      // top level dependencies, 'include' will find
+      // nested dependencies.
       include: [
         'app/models/basicModel',
         'bootstrap'
       ],
-      //Since we bundle jquery with require.js we don't need
+      // Since we bundle jquery with require.js we don't need
       // to additionally compile it into this layer. It will
       // already be available on the page as part of the 
       // script tag that pulls in require.js
-      // ex: page1.html
-      // <script src="js/vendor/require-jquery.js"></script>
       exclude: ['jquery']
     },
 
-    //Now set up a build layer for each main layer, but exclude
-    //the common one. 'exclude' will exclude nested
-    //the nested, built dependencies from 'common'. Any
-    //'exclude' that includes built modules should be
-    //listed before the build layer that wants to exclude it.
-    //The 'page1' and 'page2' modules are **not** the targets of
-    //the optimization, because shim config is in play, and
-    //shimmed dependencies need to maintain their load order.
-    //In this example, common.js will hold jquery, so backbone
-    //needs to be delayed from loading until common.js finishes.
-    //That loading sequence is controlled in page1.js.
+    // Now set up a build layer for each main layer, but exclude
+    // the common one. If you're excluding a module, the excludee
+    // must appear before the excluder in this file. Otherwise it will
+    // get confused.
     {
-      //module names are relative to baseUrl/paths config
       name: 'app/main-about',
       exclude: ['common', 'jquery']
     },
 
     {
-      //module names are relative to baseUrl
       name: 'app/main-contact',
       exclude: ['common', 'jquery']
     }
